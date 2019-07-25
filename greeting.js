1075bc198e6880ba54deebb8b4b420c564e9b685
dolmi <dolmihyun@gmail.com>
2019-07-25 18:49:35 +0900

    Update greeting.js

--- greeting.js
+++ greeting.js
@@ -1,10 +1,14 @@
 const form = document.querySelector(".js-form"),
     input = form.querySelector("input"),
     greeting = document.querySelector("js-greetings");
-const USER_LS = "currentUser"
+
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing";
 
 function paintGreeting(text) {
-
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello, ${text}`;
 }
 
 function loadName() {
@@ -12,7 +16,7 @@ function loadName() {
     if (currentUser === null) {
 
     } else {
-
+        paintGreeting(currentUser);
     }
 }
 
